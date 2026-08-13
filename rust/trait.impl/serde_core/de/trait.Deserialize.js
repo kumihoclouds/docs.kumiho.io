a5aabc58ff11fddaec97a0f212cc9797cc249645
kaveone <kaveone@users.noreply.github.com>
2026-08-13 01:01:33 +0000

    docs(rust): update to v0.10.0 KumihoIO/kumiho-SDKs@ba3adf2882af5717be5b948f8382f8a8bad7f773

--- rust/trait.impl/serde_core/de/trait.Deserialize.js
+++ rust/trait.impl/serde_core/de/trait.Deserialize.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["kumiho",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"kumiho/struct.DiscoveryRecord.html\" title=\"struct kumiho::DiscoveryRecord\">DiscoveryRecord</a>",0]]]]);
+    const implementors = Object.fromEntries([["kumiho",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.229/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"kumiho/struct.DiscoveryRecord.html\" title=\"struct kumiho::DiscoveryRecord\">DiscoveryRecord</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- rust/trait.impl/serde_core/ser/trait.Serialize.js
+++ rust/trait.impl/serde_core/ser/trait.Serialize.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["kumiho",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"kumiho/struct.DiscoveryRecord.html\" title=\"struct kumiho::DiscoveryRecord\">DiscoveryRecord</a>",0]]]]);
+    const implementors = Object.fromEntries([["kumiho",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.229/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"kumiho/struct.DiscoveryRecord.html\" title=\"struct kumiho::DiscoveryRecord\">DiscoveryRecord</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
